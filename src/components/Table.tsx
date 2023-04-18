093e1049d6b949aef23f463bb55186b2fecd8f9f
Elpah <obengelpachris@gmail.com>
2023-04-18 23:53:54 +0200

    display values onClick

--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -1,42 +1,125 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./table.css";
+
 export default function Table() {
-  const [firstNumber, setFirstNumber] = useState(0);
-  const [secondNumber, setSecondNumber] = useState(0);
+  const [firstNumber, setFirstNumber] = useState("");
+  const [secondNumber, setSecondNumber] = useState("");
   const [operator, setOperator] = useState("");
   const [result, setResult] = useState("");
 
+  //   const add = (firstNumber: number, secondNumber: number) =>
+  //     firstNumber + secondNumber;
+
+  //   const subtract = (firstNumber: number, secondNumber: number) =>
+  //     firstNumber - secondNumber;
+
+  //   const divide = (firstNumber: number, secondNumber: number) => {
+  //     if (secondNumber === 0) {
+  //       throw new Error("cannot divide by 0");
+  //     }
+  //     return firstNumber / secondNumber;
+  //   };
+  //   const multiply = (firstNumber: number, secondNumber: number) => {
+  //     return firstNumber * secondNumber;
+  //   };
+
+  useEffect(() => {
+    console.log(firstNumber);
+  }, [firstNumber]);
+
+  useEffect(() => {
+    setResult(firstNumber);
+  }, [firstNumber]);
+
+  useEffect(() => {
+    console.log(operator);
+  }, [operator]);
+
+  function handleClick(event: React.MouseEvent<HTMLTableCellElement>) {
+    if (operator === "") {
+      const firstValue = (event.target as HTMLTableCellElement).innerText;
+      setFirstNumber((prevResult) => prevResult + firstValue);
+    }
+  }
+
+  function useOperator(event: React.MouseEvent<HTMLTableCellElement>) {
+    if (firstNumber !== "") {
+      const value = (event.target as HTMLTableCellElement).innerText;
+      setOperator(value);
+      setResult((prevResult) => prevResult + value);
+    }
+  }
+
+  function handleReset() {
+    setFirstNumber("");
+    setSecondNumber("");
+    setResult("");
+    setOperator("");
+  }
+
   return (
     <>
-      <h1 className="result"></h1>
+      <h1 className="result">{result}</h1>
 
       <table>
         <tr>
-          <td className="clear">AC</td>
+          <td className="clear" onClick={handleReset}>
+            AC
+          </td>
           <td className="posNeg">+/-</td>
-          <td className="percentage">%</td>
-          <td className="operator divide">÷</td>
+          <td className="percentage" onClick={useOperator}>
+            %
+          </td>
+          <td className="operator divide" onClick={useOperator}>
+            ÷
+          </td>
         </tr>
         <tr>
-          <td className="seven">7</td>
-          <td className="eight">8</td>
-          <td className="nine">9</td>
-          <td className="operator multiply">x</td>
+          <td className="seven" onClick={handleClick}>
+            7
+          </td>
+          <td className="eight" onClick={handleClick}>
+            8
+          </td>
+          <td className="nine" onClick={handleClick}>
+            9
+          </td>
+          <td className="operator multiply" onClick={useOperator}>
+            *
+          </td>
         </tr>
         <tr>
-          <td className="foiur">4</td>
-          <td className="five">5</td>
-          <td className="six">6</td>
-          <td className="operator subtract">-</td>
+          <td className="four" onClick={handleClick}>
+            4
+          </td>
+          <td className="five" onClick={handleClick}>
+            5
+          </td>
+          <td className="six" onClick={handleClick}>
+            6
+          </td>
+          <td className="operator subtract" onClick={useOperator}>
+            -
+          </td>
         </tr>
         <tr>
-          <td className="one">1</td>
-          <td className="two">2</td>
-          <td className="three">3</td>
-          <td className="operator add">+</td>
+          <td className="one" onClick={handleClick}>
+            1
+          </td>
+          <td className="two" onClick={handleClick}>
+            2
+          </td>
+          <td className="three" onClick={handleClick}>
+            3
+          </td>
+          <td className="operator add" onClick={useOperator}>
+            +
+          </td>
         </tr>
         <tr>
-          <td colSpan={2}>0</td>
+          <td colSpan={2} onClick={handleClick}>
+            0
+          </td>
           <td className="period">.</td>
           <td className="equals">=</td>
         </tr>
